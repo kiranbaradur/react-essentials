d71a53a064fdd09a1d222465a75fd4f0876616b0
kiranbaradur <kiranbaradur@gmail.com>
2024-01-01 11:45:36 +0530

    Final changes to React Essentials.

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Header = () => {
+  const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+  }
+
+  const description = reactDescriptions[getRandomInt(2)];
+  console.log(description);
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TabButton = ({ children, isSelected, ...props }) => {
+  return (
+    <div>
+      <li>
+        <button className={isSelected ? "active" : undefined} {...props}>
+          {children}
+        </button>
+      </li>
+    </div>
+  );
+};
+
+export default TabButton;
